233843a6d45d7a3e9b13cbded775c649d862f8af
Gisela <giselatb@outlook.com>
2023-10-18 20:30:11 +0000

    add azure pipeline

--- steps.sh
+++ steps.sh
@@ -1,6 +1,11 @@
 #### Gitleaks ####
-gitleaks detect 
-
+gitleaks detect --source . --redact
 
 #### detect-secrets ###
-detect-secrets scan
+detect-secrets scan --all-files
+
+### ggshield ###
+GITGUARDIAN_API_KEY="F12928a8d4B11AF6c1C8f0589CC46DaCc5BceFEa217Bcf5941d27Ac1B5FFE5e8735c202"
+ggshield secret scan path -r .
+### Trufflehog ###
+trufflehog filesystem .
