3363eb75849e6cad4f2d901223c020fddf9022a0
Gisela <giselatb@outlook.com>
2023-10-18 20:58:17 +0000

    add more secrets for testing

--- steps.sh
+++ steps.sh
@@ -3,10 +3,9 @@ gitleaks detect
 gitleaks protect -v
 
 #### detect-secrets ###
-detect-secrets scan --all-files
+detect-secrets scan
 
 ### ggshield ###
-GITGUARDIAN_API_KEY="F12928a8d4B11AF6c1C8f0589CC46DaCc5BceFEa217Bcf5941d27Ac1B5FFE5e8735c202"
 ggshield secret scan path -r .
 ### Trufflehog ###
 trufflehog filesystem .
