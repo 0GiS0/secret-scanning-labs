e106a857a32d9665b4bb5f638264208de828e381
0GiS0 <giselatb@outlook.com>
2023-10-19 13:14:15 +0200

    short tools in steps.sh

--- steps.sh
+++ steps.sh
@@ -1,9 +1,9 @@
 #### detect-secrets ###
 detect-secrets scan
 
+### Trufflehog ###
+trufflehog filesystem .
+
 ### ggshield ###
 ggshield auth login
 ggshield secret scan path -r . -y
-
-### Trufflehog ###
-trufflehog filesystem .
